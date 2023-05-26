99efc1e89123626be69580c38731ef586be320db
BuddyWood Talton <buddy.talton@outlook.com>
2023-05-25 20:37:22 -0500

    create job for 1kappa

--- createJobs.groovy
+++ createJobs.groovy
@@ -5,4 +5,33 @@ pipelineJob('pipelineJob') {
             sandbox()
         }
     }
+
+pipelineJob('1kappa-job') {
+        definition {
+            cspScm {
+                git {
+                    remote {
+                        url 'https://github.com/buddywood/OneKappaWeb.git'
+                    }
+                    branch 'main'
+                }
+            }
+
+        }
+    }
+    pipelineJob('1kappa-job-docker') {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url 'https://github.com/buddywood/OneKappaWeb.git'
+                        }
+                        branch 'main'
+                        scriptPath('Jenkinsfile-docker')
+                    }
+                }
+            }
+        }
+    }
 }
